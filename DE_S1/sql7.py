5ae74a3bb7468022b64df2a94a17d547cbaca5dd
SantiData87 <137358032+SantiData87@users.noreply.github.com>
2024-05-07 19:39:52 -0300

    Delete DE_S1 directory

--- DE_S1/sql7.py
+++
@@ -1,4 +0,0 @@
-#Escribir una consulta que extraiga dos métricas del desempeño de los agentes de ventas que le interesan a su empresa: 
-#Para cada agente, cuántos segundos en promedio les toma vender un producto cuando tienen éxito.
-#Para cada agente, cuántos segundos en promedio permanecen en el teléfono antes de darse por vencidos cuando no tienen éxito.
-
